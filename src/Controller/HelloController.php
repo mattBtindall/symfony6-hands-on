5cdee929f3ed7db9470490616bc684daf5bd18db
mattbtindall <matthewbtindall@gmail.com>
2023-04-13 09:26:20 +0100

    Learn: Twig loops and conditionals

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    private array $messages = [
+        "Hello", "Hi", "Bye!"
+    ];
+
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): Response
+    {
+        return $this->render(
+            '/hello/index.html.twig',
+            ['messages' => array_slice($this->messages, 0, $limit)]
+        );
+    }
+
+    #[Route('/show/{id<\d+>}', name: 'app_show')]
+    public function show(int $id): Response
+    {
+        return $this->render(
+            'hello/show.html.twig',
+            ['message' => $this->messages[$id]]
+        );
+    }
+}
