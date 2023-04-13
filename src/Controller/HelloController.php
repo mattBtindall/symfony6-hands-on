cd6453f16530697b565001ab92ae44ec956d5382
mattbtindall <matthewbtindall@gmail.com>
2023-04-13 09:52:35 +0100

    Remove slice from control
    Performs slice within the twig templates
    This keeps the controller clean - it loads and passes the data to the correct template

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -17,7 +17,11 @@ class HelloController extends AbstractController
     {
         return $this->render(
             '/hello/index.html.twig',
-            ['messages' => array_slice($this->messages, 0, $limit)]
+            [
+                'messages' => $this->messages,
+                'limit' => $limit
+            ]
+
         );
     }
 
