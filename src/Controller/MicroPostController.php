05c723c67678806ed26121f1516db7285b05dcc2
mattBtindall <matthewbtindall@gmail.com>
2023-04-20 09:31:01 +0100

    Add micropost controller methods and templates

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,24 +13,16 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
-        // Add new entity
-        // $microPostNew = new MicroPost();
-        // $microPostNew->setTitle('It comes from controller');
-        // $microPostNew->setText('Hi');
-        // $microPostNew->setCreated(new DateTime());
-        // $posts->save($microPostNew, true);
-
-        // Update entity
-        // $microPostUpdate = $posts->find(1);
-        // $microPostUpdate->setTitle('Welcome to Findland');
-        // $posts->save($microPostUpdate, true);
-
-        // Delete entity
-        // $microPostDelete = $posts->find(4);
-        // $posts->remove($microPostDelete, true);
-
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll()
+        ]);
+    }
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function show(MicroPost $post): Response
+    {
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post
         ]);
     }
 }
