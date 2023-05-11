47ace2cf6a96d38f45ed34aa47ced5034e705810
mattBtindall <matthewbtindall@gmail.com>
2023-05-11 09:12:27 +0100

    Embed user_profile form within registration form

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SettingsProfileController extends AbstractController
 {
     #[Route('/settings/profile', name: 'app_settings_profile')]
-    public function profile(Request $request): Response
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function profile(
+        Request $request,
+        UserRepository $users
+    ): Response
     {
         /** @var User $user */
         $user = $this->getUser();
@@ -25,9 +32,14 @@ class SettingsProfileController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userProfile = $form->getData();
-            // save this
-            // Add flash message
+            $user->setUserProfile($userProfile);
+            $users->save($user, true);
+            $this->addFlash(
+                'success',
+                'Your user profile was updated'
+            );
             // Redirect
+            return $this->redirectToRoute('app_settings_profile');
         }
 
         return $this->render(
