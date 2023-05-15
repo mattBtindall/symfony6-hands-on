7cb34deb6a0bf51d5d8e2918359a924e8df95357
mattBtindall <matthewbtindall@gmail.com>
2023-05-15 08:18:06 +0100

    Add tabs to homepage
    This simply adds the tabs with the routes but the pages don't contain the correct info

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -25,6 +25,22 @@ class MicroPostController extends AbstractController
         ]);
     }
 
+    #[Route('/micro-post/top-liked', name: 'app_micro_post_topliked')]
+    public function topliked(MicroPostRepository $posts): Response
+    {
+        return $this->render('micro_post/topliked.html.twig', [
+            'posts' => $posts->findAllWithComments()
+        ]);
+    }
+
+    #[Route('/micro-post/follows', name: 'app_micro_post_follows')]
+    public function follows(MicroPostRepository $posts): Response
+    {
+        return $this->render('micro_post/follows.html.twig', [
+            'posts' => $posts->findAllWithComments()
+        ]);
+    }
+
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
     #[IsGranted(MicroPost::VIEW, 'post')]
     public function show(MicroPost $post): Response
