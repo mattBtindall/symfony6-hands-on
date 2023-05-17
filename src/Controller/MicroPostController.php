3918a07c93292e98b773335b5af46e2fa04206f4
mattBtindall <matthewbtindall@gmail.com>
2023-05-17 17:01:03 +0100

    Add extra privacy voter

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -54,7 +54,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
-    #[IsGranted('ROLE_WRITER')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function add(Request $request, MicroPostRepository $posts): Response
     {
         $form = $this->createForm(MicroPostType::class, new MicroPost());
--- src/Security/Voter/MicroPostVoter.php
+++ src/Security/Voter/MicroPostVoter.php
@@ -52,9 +52,12 @@ class MicroPostVoter extends Voter
                         $this->security->isGranted('ROLE_EDITOR')
                     );
             case MicroPost::VIEW:
-                // logic to determine if the user can VIEW
-                // return true or false
-                return true;
+                if (!$subject->isExtraPrivacy()) return true;
+
+                return $isAuth &&
+                    ($subject->getAuthor()->getId() === $user->getId() ||
+                        $subject->getAuthor()->getFollows()->contains($user)
+                    );
         }
 
         return false;
